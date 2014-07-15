9005bad1ccae07ccd95e26a6d29f88beedac90a6
Konstantin Shtompel <konstantin.shtompel@blinkreaction.com>
2014-07-15 13:36:10 +0300

    Helper commit)

--- framework/Helper.php
+++ framework/Helper.php
@@ -25,26 +25,26 @@ class Helper
 	}
 
 	//render template
-	public function function_get_output($fn)
+	public static function function_get_output($fn)
 	{
 	  $args = func_get_args();unset($args[0]);
 	  ob_start();
-	  call_user_func_array(array($this, $fn), $args);
+	  call_user_func_array('self::' . $fn, $args);
 	  $output = ob_get_contents();
 	  ob_end_clean();
 	  return $output;
 	}
 
-	public function display($template, $params = array())
+	public static function display($template, $params = array())
 	{
 	  extract($params);
 	  // require $template;
 	  include $template;
 	}
 
-	public function render($template, $params = array())
+	public static function render($template, $params = array())
 	{
-	  return $this->function_get_output('display', $template, $params);
+	  return self::function_get_output('display', $template, $params);
 	}
 
   public static function stringLimitWords($string, $word_limit)
