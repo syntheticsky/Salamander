947fa8a0320b2f2254fd1b95fdd55f4941235da4
Konstantin Shtompel <konstantin.shtompel@blinkreaction.com>
2014-07-15 18:40:08 +0300

    Added widgets Contacts, Flickr, Facebook like, Ads125x125, Recent Works.

--- framework/libs/Widgets.php
+++ framework/libs/Widgets.php
@@ -2,15 +2,20 @@
 
 class Widgets
 {
-  function __construct()
+  public function __construct()
   {
     add_action('widgets_init', array($this, 'loadWidgets'));
   }
 
-  function loadWidgets()
+  public function loadWidgets()
   {
     register_widget('TabsWidget');
     register_widget('TweetsWidget');
     register_widget('SocialLinksWidget');
+    register_widget('ContactInfoWidget');
+    register_widget('FacebookLikeWidget');
+    register_widget('FlickrWidget');
+    register_widget('Ad125x125Widget');
+    register_widget('RecentWorksWidget');
   }
 }
